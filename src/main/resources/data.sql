e4aadfcf451e389b3418f8712d841b6d1f69a880
Mattia Andrea <An.Mattia@almaviva.it>
2026-01-17 14:28:34 +0100

    Fix syntax MERGE INTO in data.sql per H2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-MERGE INTO tipo_ruolo KEY (codice) VALUES('1', 'Gancio');
-MERGE INTO tipo_ruolo KEY (codice) VALUES('2', 'Gira teste');
-MERGE INTO tipo_ruolo KEY (codice) VALUES('3', 'Sfogliatura');
-MERGE INTO tipo_ruolo KEY (codice) VALUES('4', 'Femminelle');
-MERGE INTO tipo_ruolo KEY (codice) VALUES('5', 'Fiori');
-MERGE INTO tipo_ruolo KEY (codice) VALUES('6', 'Raccolta');
+MERGE INTO tipo_ruolo (codice, descrizione) KEY (codice) VALUES('1', 'Gancio');
+MERGE INTO tipo_ruolo (codice, descrizione) KEY (codice) VALUES('2', 'Gira teste');
+MERGE INTO tipo_ruolo (codice, descrizione) KEY (codice) VALUES('3', 'Sfogliatura');
+MERGE INTO tipo_ruolo (codice, descrizione) KEY (codice) VALUES('4', 'Femminelle');
+MERGE INTO tipo_ruolo (codice, descrizione) KEY (codice) VALUES('5', 'Fiori');
+MERGE INTO tipo_ruolo (codice, descrizione) KEY (codice) VALUES('6', 'Raccolta');
